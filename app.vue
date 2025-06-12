c7f1c22aff3e87ba55793989fc66aea6e13b7d3f
workwithmerakiart <workwithmerakiart@gmail.com>
2025-06-12 07:33:05 +0530

    Second commit by DS for Footer and Hero

--- app.vue
+++ app.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
+  <div class="min-h-screen flex flex-col">
     <AppHeader />
+
+    <!-- Main content grows to push footer down -->
+    <div class="flex-1">
+      <HeroSection />
+      <NuxtPage />
+    </div>
+
+    <AppFooter />
   </div>
 </template>
-
