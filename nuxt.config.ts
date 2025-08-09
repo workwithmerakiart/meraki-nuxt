703d2977a142d6cdedadc5974fd30c8e4f46b8d3
workwithmerakiart <workwithmerakiart@gmail.com>
2025-08-09 14:51:01 +0530

    Deploy SSR

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
+  ssr: true,
   nitro: { preset: 'netlify' },
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
