3ec0b1428f2a491d893a1e41cbf7507060a06ffa
Deepak Sengar <deepak.sengar@macys.com>
2025-08-25 18:15:16 +0530

    no CTA custom orders

--- pages/shop/custom-orders.vue
+++ pages/shop/custom-orders.vue
@@ -28,10 +28,6 @@
         <div class="md:w-1/2 w-full">
           <h3 class="text-xl sm:text-2xl font-bold mb-2">{{ item.title }}</h3>
           <p class="text-gray-700 mb-4">{{ item.description }}</p>
-          <button
-            class="inline-block border border-black text-black px-4 py-2 rounded hover:bg-[#447c9d] hover:text-white transition">
-            Add to Cart
-          </button>
         </div>
       </div>
     </div>
