76e746d000adc635cc6a0b18f0e6654a95a3e11f
workwithmerakiart <workwithmerakiart@gmail.com>
2026-05-01 10:35:14 +0100

    Line fix

--- components/home/BlockFour.vue
+++ components/home/BlockFour.vue
@@ -3,7 +3,7 @@
     <!-- Heading -->
     <div class="heading-bg p-2.5 mb-5">
       <h2 class="heading text-5xl font-bold text-[#447c9d] inter-font">
-        POPULAR ACTIVITIES
+        JOIN OUR UPCOMING EVENTS
       </h2>
     </div>
 
